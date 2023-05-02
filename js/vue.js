89c186bbf0b595e23988660ec3a3f8831d28f098
LiangFeng111 <2629337569@qq.com>
2023-05-02 22:37:35 +0800

    Site updated: 2023-05-02 22:37:28

---
+++ js/vue.js
@@ -0,0 +1,158 @@
+// 防抖全局计时器
+let TT = null;    //time用来控制事件的触发
+// 防抖函数:fn->逻辑 time->防抖时间
+function debounce(fn, time) {
+    if (TT !== null) clearTimeout(TT);
+    TT = setTimeout(fn, time);
+}
+
+
+
+document.onkeydown = function(e) {
+    if (123 == e.keyCode || e.ctrlKey && e.shiftKey && (74 === e.keyCode || 73 === e.keyCode || 67 === e.keyCode) || e.ctrlKey && 85 === e.keyCode){
+        debounce(function () {
+            new Vue({
+                data: function () {
+                    this.$notify({
+                        title: "你已被发现😜",
+                        message: "小伙子，扒源记住要遵循GPL协议！",
+                        position: 'top-left',
+                        offset: 50,
+                        showClose: true,
+                        type: "warning",
+                        duration: 5000
+                    });
+                }
+            })
+        },500)
+    }
+}
+
+document.addEventListener('copy', function(event) {
+    debounce(function () {
+        new Vue({
+            data: function () {
+                this.$notify({
+                    title: "哎嘿！复制成功🍬",
+                    message: "若要转载最好保留原文链接哦，给你一个大大的赞！",
+                    position: "top-left",
+                    offset: 50,
+                    showClose: !0,
+                    type: "success",
+                    duration: 5e3
+                })
+            }
+        })
+    },500)
+});
+
+// 分享本页
+function share_() {
+    let url = window.location.origin + window.location.pathname
+    try {
+        // 截取标题
+        var title = document.title;
+        var subTitle = title.endsWith("凉风blog") ? title.substring(0, title.length - 5) : title;
+        navigator.clipboard.writeText('凉风博客的站内分享\n标题：' + subTitle + '\n链接：' + url + '\n欢迎来访！🍭🍭🍭');
+        new Vue({
+            data: function () {
+                this.$notify({
+                    title: "成功复制分享信息🎉",
+                    message: "您现在可以通过粘贴直接跟小伙伴分享了！",
+                    position: 'top-left',
+                    offset: 50,
+                    showClose: true,
+                    type: "success",
+                    duration: 5000
+                });
+                // return { visible: false }
+            }
+        })
+    } catch (err) {
+        console.error('复制失败！', err);
+    }
+    // new ClipboardJS(".share", { text: function () { return '标题：' + document.title + '\n链接：' + url } });
+    // btf.snackbarShow("本页链接已复制到剪切板，快去分享吧~")
+}
+
+// 防抖
+function share() {
+    debounce(share_, 300);
+}
+
+
+function switchNightMode() {
+    document.querySelector('body').insertAdjacentHTML('beforeend', '<div class="Cuteen_DarkSky"><div class="Cuteen_DarkPlanet"><div id="sun"></div><div id="moon"></div></div></div>'),
+        setTimeout(function () {
+            document.querySelector('body').classList.contains('DarkMode') ? (document.querySelector('body').classList.remove('DarkMode'), localStorage.setItem('isDark', '0'), document.getElementById('modeicon').setAttribute('xlink:href', '#icon-moon')) : (document.querySelector('body').classList.add('DarkMode'), localStorage.setItem('isDark', '1'), document.getElementById('modeicon').setAttribute('xlink:href', '#icon-sun')),
+                setTimeout(function () {
+                    document.getElementsByClassName('Cuteen_DarkSky')[0].style.transition = 'opacity 3s';
+                    document.getElementsByClassName('Cuteen_DarkSky')[0].style.opacity = '0';
+                    setTimeout(function () {
+                        document.getElementsByClassName('Cuteen_DarkSky')[0].remove();
+                    }, 1e3);
+                }, 2e3)
+        })
+    const nowMode = document.documentElement.getAttribute('data-theme') === 'dark' ? 'dark' : 'light'
+    if (nowMode === 'light') {
+        // 先设置太阳月亮透明度
+        document.getElementById("sun").style.opacity = "1";
+        document.getElementById("moon").style.opacity = "0";
+        setTimeout(function () {
+            document.getElementById("sun").style.opacity = "0";
+            document.getElementById("moon").style.opacity = "1";
+        }, 1000);
+
+        activateDarkMode()
+        saveToLocal.set('theme', 'dark', 2)
+        // GLOBAL_CONFIG.Snackbar !== undefined && btf.snackbarShow(GLOBAL_CONFIG.Snackbar.day_to_night)
+        document.getElementById('modeicon').setAttribute('xlink:href', '#icon-sun')
+        // 延时弹窗提醒
+        setTimeout(() => {
+            new Vue({
+                data: function () {
+                    this.$notify({
+                        title: "关灯啦🌙",
+                        message: "当前已成功切换至夜间模式！",
+                        position: 'top-left',
+                        offset: 50,
+                        showClose: true,
+                        type: "success",
+                        duration: 5000
+                    });
+                }
+            })
+        }, 2000)
+    } else {
+        // 先设置太阳月亮透明度
+        document.getElementById("sun").style.opacity = "0";
+        document.getElementById("moon").style.opacity = "1";
+        setTimeout(function () {
+            document.getElementById("sun").style.opacity = "1";
+            document.getElementById("moon").style.opacity = "0";
+        }, 1000);
+
+        activateLightMode()
+        saveToLocal.set('theme', 'light', 2)
+        document.querySelector('body').classList.add('DarkMode'), document.getElementById('modeicon').setAttribute('xlink:href', '#icon-moon')
+        setTimeout(() => {
+            new Vue({
+                data: function () {
+                    this.$notify({
+                        title: "开灯啦🌞",
+                        message: "当前已成功切换至白天模式！",
+                        position: 'top-left',
+                        offset: 50,
+                        showClose: true,
+                        type: "success",
+                        duration: 5000
+                    });
+                }
+            })
+        }, 2000)
+    }
+    // handle some cases
+    typeof utterancesTheme === 'function' && utterancesTheme()
+    typeof FB === 'object' && window.loadFBComment()
+    window.DISQUS && document.getElementById('disqus_thread').children.length && setTimeout(() => window.disqusReset(), 200)
+}
