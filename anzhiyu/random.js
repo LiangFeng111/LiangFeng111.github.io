1df48939419660f37a1f18e5ca20996fe1ecb350
LiangFeng111 <87523187+LiangFeng111@users.noreply.github.com>
2026-03-27 00:04:18 +0000

    更新所有配置 Updated By Github Actions

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/03/26/hello-world/","2026/03/26/测试/"];function toRandomPost(){
+var posts=["2026/03/27/hello-world/","2026/03/26/测试/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
